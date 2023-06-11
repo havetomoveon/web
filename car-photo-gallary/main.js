641c4acbd276a1fafa75a2ced6690563c2aa7e48
havetomoveon <safdarian23@gmail.com>
2023-06-11 21:55:30 +0330

    car-photo-gallary/main.js

---
+++ car-photo-gallary/main.js
@@ -0,0 +1,23 @@
+let num_file = 0;
+let tr;
+let ele = document.getElementById("listImageTable");
+for(let d = 0; d < 11; d++){
+    
+    let td = document.createElement("td");
+    let img = document.createElement("img");
+    td.className = "ImageItem";
+    img.className = "image";
+    img.src = `images/images_${d}.jpg`;
+    td.appendChild(img);
+    if (d % 4 == 0){
+        tr = document.createElement("tr");
+        tr.appendChild(td);
+        ele.appendChild(tr);
+        ele.style.height = `${d * 100}px`;   
+    } else {
+        tr = ele.lastChild;
+        tr.appendChild(td);
+    }
+
+
+}
